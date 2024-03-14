83f75e580de02d5987f85aa6a56dbf26de1f1533
Zaahira <herbold.christine83@gmail.com>
2024-03-14 11:09:14 +0100

    update

--- src/Form/Type/EditType.php
+++ src/Form/Type/EditType.php
@@ -3,6 +3,7 @@
 namespace App\Form\Type;
 
 use App\FinanceType;
+use App\Entity\Finance;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -11,6 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class EditType extends AbstractType{
 
@@ -23,6 +25,13 @@ class EditType extends AbstractType{
             ->add('date', DateType::class)
             ->add('save', SubmitType::class)
             ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver){
+        $resolver->setDefaults([
+            'data_class'=> Finance::class,
+            'csrf_protection'=> true,
+        ]);
     }
 
 }
